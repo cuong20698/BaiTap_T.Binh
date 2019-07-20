e2a0a285d110298801a71edea61ba3a19aae61f4
cuong20698 <52898543+cuong20698@users.noreply.github.com>
2019-07-20 22:50:47 +0700

    Cập nhật BaiTap1
    Viết code cho GuestBook.aspx.cs

--- BaiTap1/GuestBook.aspx.cs
+++ BaiTap1/GuestBook.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -13,6 +14,16 @@ public partial class GuestBook : System.Web.UI.Page
     }
     protected void btnGuiND_Click(object sender, EventArgs e)
     {
-        
+        string sfile = Server.MapPath(@"\") + "data.txt";
+
+        using (StreamWriter writer = new StreamWriter(sfile, true))
+        {
+            writer.WriteLine(txtTieuDe.Text);
+            writer.WriteLine(txtHoTen.Text);
+            writer.WriteLine(txtEmail.Text);
+            writer.WriteLine(txtNoiDung.Text);
+            writer.WriteLine("#END");
+            writer.Close();
+        }
     }
 }
