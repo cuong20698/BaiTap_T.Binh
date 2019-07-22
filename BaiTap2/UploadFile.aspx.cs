a56488d404f1dd28c32aef3ffa6b2bcb25435354
cuong20698 <52898543+cuong20698@users.noreply.github.com>
2019-07-22 10:14:19 +0700

    Sửa code UploadFile.aspx.cs

--- BaiTap2/UploadFile.aspx.cs
+++ BaiTap2/UploadFile.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -22,10 +23,26 @@ public partial class UploadFile : System.Web.UI.Page
     }
     protected void btnUpload_Click(object sender, EventArgs e)
     {
-        if (btnFile.HasFile)
+        if (btnFile.HasFile && CheckFileType(btnFile.FileName))
         {
             string fileName = @"\FileUpload\" + btnFile.FileName;
             btnFile.SaveAs(MapPath(fileName));
+        }
+    }
+
+    private bool CheckFileType(string fileName)
+    {
+        string check = Path.GetExtension(fileName);
+        switch (check.ToLower())
+        {
+            case ".doc":
+                return true;
+            case ".docx":
+                return true;
+            case ".jpg":
+                return true;
+            default:
+                return false;
         }
     }
 }
