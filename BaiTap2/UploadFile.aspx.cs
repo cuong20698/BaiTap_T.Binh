f393b8aef8deacbffc22504f791f0a422d07d6b5
cuong20698 <52898543+cuong20698@users.noreply.github.com>
2019-07-22 09:20:11 +0700

    Tạo project BatTap2
    Thêm mới và thiết kế giao diện file Upload.aspx

---
+++ BaiTap2/UploadFile.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class UploadFile : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        MultiView1.ActiveViewIndex = 0;
+    }
+
+    protected void LinkUpload_Click(object sender, EventArgs e)
+    {
+        MultiView1.ActiveViewIndex = 0;
+    }
+    protected void LinkXemUpload_Click(object sender, EventArgs e)
+    {
+        MultiView1.ActiveViewIndex = 1;
+    }
+}
