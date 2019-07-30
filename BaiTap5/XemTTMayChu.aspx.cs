2b32c578e78b7f46ec0ea9c8f9bddf992fb1e21f
cuong20698 <52898543+cuong20698@users.noreply.github.com>
2019-07-30 10:03:16 +0700

    Ứng dụng xem thông tin máy chủ

---
+++ BaiTap5/XemTTMayChu.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class XemTTMayChu : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string nameserver = Server.MachineName;
+        string linkserver = Server.MapPath(@"\") + "XemTTMayChu.aspx";
+        Response.Write("Name server: "+nameserver+"</br>"+"Link: " +linkserver);
+    }
+}
